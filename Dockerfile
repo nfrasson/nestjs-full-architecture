d755c7c93dedf24ca4ab8fc8b0b582284320bc14
Nicolas Frasson <nicfra0710@gmail.com>
2024-01-28 14:05:14 -0300

    imp: add hot reload to development environment + nginx with application replicas

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN npm ci --omit=dev --ignore-scripts
 COPY . .
 RUN npm run build
 
-FROM node:20
+FROM node:20-slim
 WORKDIR /app
 COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/dist ./dist
--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap(): Promise<void> {
   app.register(helmet);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(process.env.HTTP_PORT || 3000, '0.0.0.0');
 }
 bootstrap();
